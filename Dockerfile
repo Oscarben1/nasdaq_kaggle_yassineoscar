5fdd11d386d70fdb279f6fceb1c789d3c0a8ba32
Oscar Benoit <oscarbenoit@MacBook-Air-de-Oscar-2.local>
2023-12-07 11:09:23 +0100

    new version api

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,11 @@
-# Dockerfile
+FROM python:3.11
 
-# Utilisez une image de base avec Python
-FROM python:3.8
+COPY . /app
 
-# Définissez le répertoire de travail dans le conteneur
 WORKDIR /app
 
-# Copiez les fichiers nécessaires dans le conteneur
-COPY src/app.py /app/
-COPY model/testmodel.pkl /app/
-# Ajoutez des paquets système nécessaires
-RUN apt-get update && \
-    apt-get install -y libgomp1
+RUN pip install --upgrade pip
 
-# Copiez le fichier requirements.txt et installez les dépendances
-COPY requirements.txt /app/
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install -r requirements.txt
 
-# Exposez le port sur lequel l'application Flask va écouter
-EXPOSE 5001
-
-# Commande pour lancer l'application Flask
-CMD ["python", "app.py", "--host=0.0.0.0", "--port=5001"]
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "80"]
--- src/app.py
+++ src/app.py
@@ -1,47 +1,71 @@
-from flask import Flask, request, jsonify
-import pickle
-import catboost as cb
+import json
+import joblib
 import pandas as pd
-import numpy as np
-from sklearn import LinearRegressor, train_test_split
+import tensorflow as tf
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field
+from typing import Optional
 
-app = Flask(__name__)
+class StockData(BaseModel):
+    stock_id: int
+    date_id: int
+    seconds_in_bucket: int
+    imbalance_size: float
+    imbalance_buy_sell_flag: int
+    reference_price: float
+    matched_size: float
+    far_price: Optional[float] = None
+    near_price: Optional[float] = None
+    bid_price: float
+    bid_size: float
+    ask_price: float
+    ask_size: float
+    wap: float
+    time_id: int
+    row_id: str
 
-model_path = "testmodel.pkl"  # Assurez-vous d'avoir le bon chemin vers votre modèle CatBoost
-with open(model_path, "rb") as model_file:
-    catboost_model = pickle.load(model_file)
+app = FastAPI()
 
-def preprocess(df):
-    df_pd = pd.read_csv(df)
-    # Ajoutez ici des étapes de prétraitement si nécessaire
-    return df_pd
+scaler = joblib.load('scaler.pkl')
+model = tf.keras.models.load_model('.')
 
-@app.route('/')
-def welcome():
-    return 'Bienvenue'
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/docs")
 
-@app.route('/predict', methods=['GET'])
-def predict():
-    try:
-        # Obtenez les données d'entrée de la requête GET
-        data = request.args.get('data')
+@app.get("/predict")
+def predict(data: StockData):
+    if 'far_price' in data:
+        del data['far_price']
 
-        # Prétraitement des données
-        data = preprocess(data)
+    if 'near_price' in data:
+        del data['near_price']
 
-        # Faire la prédiction avec le modèle CatBoost
-        prediction = catboost_model.predict(data)
+    # gestion d'erreur necessaire ici
+    keys_to_check = ["imbalance_size", "reference_price", "matched_size", "bid_price", "ask_price", "wap"]
 
-        # Retournez la prédiction au format JSON
-        return jsonify({'prediction': prediction.tolist()})
+    # Check if any key has an empty value
+    if any(data[key] in [None, ''] for key in keys_to_check if key in data):
+        return ValueError("Missing values for keys: {}".format(", ".join(key for key in keys_to_check if key in data)))
 
-    except Exception as e:
-        # En cas d'erreur, retournez un message d'erreur au format JSON
-        return jsonify({'error': str(e)})
+    data['imbalance_ratio'] = data['imbalance_size']/data['matched_size']
 
-# Point d'entrée pour l'application Flask
-if __name__ == '__main__':
-    # Lancer l'application sur le port 5001
-    app.run(host='0.0.0.0', port=5001)
+    data['spread_size'] = data['ask_size']-data['bid_size']
+    data['spread_size_ratio'] = data['ask_size']/data['bid_size']
 
-#http://127.0.0.1:5001 IP
+    data['bid_value'] = data['bid_price']*data['bid_size']
+    data['ask_value'] = data['ask_price']*data['ask_size']
+
+    data['spread_price'] = (data['ask_price']-data['bid_price'])/(data['ask_price'])
+
+    data['wap_spread'] = ((data['bid_price']*data['bid_size'])+(data['ask_price']*data['ask_size']))/(data['bid_size']+data['ask_size'])
+
+    df = pd.DataFrame([data])
+    df = df.set_index('stock_id')
+
+    scaled = scaler.transform(df)
+    
+    prediction = model.predict(scaled)
+
+    return JSONResponse(prediction[0][0])
